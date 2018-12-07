2e1b06144c38c2fde5ea57cf99e6f3ed4b37178c
tcrawley44 <tcrawley44@gmail.com>
2018-12-06 20:44:16 -0500

    deploy 2

--- server.js
+++ server.js
@@ -11,15 +11,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// server static assets if in production
-if(process.env.NODE_ENV === "production"){
-    //set static folder
-    app.use(express.static('client/build'));
-
-    app.get("*", (req,res) =>{
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
 
 
 
@@ -46,6 +37,16 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profiles', profiles);
 app.use('/api/posts', posts);
+
+// server static assets if in production
+if(process.env.NODE_ENV === "production"){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
